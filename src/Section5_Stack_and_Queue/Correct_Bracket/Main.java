f21dea28e686975a2926218f9de5ea7729295d92
hyojin <kimhj9292@gmail.com>
2022-03-18 00:24:05 +0900

    study up to Saving Princess

---
+++ src/Section5_Stack_and_Queue/Correct_Bracket/Main.java
@@ -0,0 +1,31 @@
+package Section5_Stack_and_Queue.Correct_Bracket;
+
+import java.util.Scanner;
+import java.util.Stack;
+public class Main {
+
+    public String solution(String str) {
+        String answer = "YES";
+        Stack<Character> stack = new Stack<>();
+        for (char x : str.toCharArray()) {
+            if (x == '(') {
+                stack.push(x);
+            } else if (stack.isEmpty()) {
+                return "NO";
+            } else {
+                stack.pop();
+            }
+        }
+        if (!stack.isEmpty()) {
+            answer = "NO";
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(T.solution(str));
+    }
+}
---
+++ src/Section5_Stack_and_Queue/Saving_Princess/Main.java
@@ -0,0 +1,32 @@
+package Section5_Stack_and_Queue.Saving_Princess;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Queue;
+
+public class Main {
+
+    public int solution(int n, int k) {
+        int answer = 0;
+        Queue<Integer> q = new LinkedList<>();
+        for (int i=0; i<n; i++) {
+            q.offer(i+1);
+        }
+        while (!q.isEmpty()) {
+            for (int i=1; i<k; i++) {
+                q.offer(q.poll());
+            }
+            q.poll();
+            if (q.size() == 1) answer = q.poll();
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(T.solution(n, k));
+    }
+}
