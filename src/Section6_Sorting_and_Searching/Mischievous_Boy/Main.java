144974abb5e8d7828e7096592c82979111ea735d
hyojin <kimhj9292@gmail.com>
2022-03-22 01:26:20 +0900

    study up to Coordinate Alignment

---
+++ src/Section6_Sorting_and_Searching/Mischievous_Boy/Main.java
@@ -0,0 +1,40 @@
+package Section6_Sorting_and_Searching.Mischievous_Boy;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int[] tmp = arr.clone();
+        for (int i=0; i<n-1; i++) {
+           int idx = i, j;
+           for (j=i+1; j<n; j++) {
+               if (arr[idx] > arr[j]) {
+                   idx = j;
+               }
+            }
+           int t = arr[i];
+           arr[i] = arr[idx];
+           arr[idx] = t;
+        }
+        for (int i=0; i<n; i++) {
+            if (arr[i] != tmp[i]) answer.add(i+1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
